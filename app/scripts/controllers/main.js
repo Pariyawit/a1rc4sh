01863dbc02c8cff9602cb1e7e7f424d7ebf63134
Pariyawit <pariyawit.jat@gmail.com>
2016-06-18 09:39:11 +0700

    no message

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aircashApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aircashApp
+ */
+angular.module('aircashApp')
+  .controller('MainCtrl', [ '$http', '$location',
+    function ($http, $location) {
+      // var req = {
+      //   method: 'GET',
+      //   url: 'http://localhost:8080/user'
+      // };
+      // $http(req).then(function(res){
+      //   console.log(res);
+      //   if(res.data.user) {
+      //
+      //   }
+      //   else {
+      //     $location.path('/login');
+      //   }
+      // }, function(error) {
+      //   console.log(error);
+      // });
+    }
+  ]);
