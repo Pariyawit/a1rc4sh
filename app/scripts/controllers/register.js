aff75b086b73b2020b526c613e4e500d43718ff1
Pariyawit <pariyawit.jat@gmail.com>
2016-06-19 02:22:06 +0700

    contact info page

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -3,7 +3,7 @@
 angular.module('aircashApp')
   .controller('RegisterCtrl', ['$scope', '$http', '$location',
     function ($scope, $http, $location) {
-
+    	$scope["page"] = 'register';
       $scope.go = function(path){
         $location.path(path);
       }
